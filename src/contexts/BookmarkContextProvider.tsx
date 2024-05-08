b26046982df28f42aa15c54e0cd5aeea70b01cdd
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-05-08 17:56:07 +0530

    fix bug

--- src/contexts/BookmarkContextProvider.tsx
+++ src/contexts/BookmarkContextProvider.tsx
@@ -5,7 +5,7 @@ import { JobItemExpendend } from "../lib/types";
 type BookMarkContext = {
   bookMarkedIds: number[];
   handleToggleBookMark: (id: number) => void;
-  bookmarkJobItems: JobItemExpendend[] | null;
+  bookmarkJobItems: JobItemExpendend[] | undefined;
   isLoading: boolean;
 };
 
